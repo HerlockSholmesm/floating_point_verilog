2ca43eb0956a52ea48e3689cfc47970efb00a06b
jamshidi.m799 <jamshidi.m799@gmail.com>
2020-08-02 16:47:17 +0430

    generate input and output

--- div/div.py
+++ div/div.py
@@ -1,4 +1,5 @@
 import struct
+import random
 
 # SIGN_INDEX = 31
 # MANTIS_INDEX = 22
@@ -15,9 +16,10 @@ class IEEE:
         number = floatToBinary64(number)
         print(number)
         self.sign = int(number[SIGN_INDEX])
-        mantis = '1' + number[MANTIS_INDEX:]
-        self.mantis = int(mantis, 2)
+        self.mantis = '1' + number[MANTIS_INDEX:]
+        # self.mantis = int(mantis, 2)
         self.exponent = int(number[1: MANTIS_INDEX], 2) - BIAS
+        # self.exponent = number[1: MANTIS_INDEX]
 
     def __str__(self):
         return 'm: {}, exp: {}, sign: {}'.format(self.mantis, self.exponent, self.sign)
@@ -28,11 +30,14 @@ class IEEE:
     def get_decimal(self):
         pass
 
+    def get_binary32(self):
+        pass
+
 
 class Div:  # a / b
     def __init__(self, a, b):
-        self.a = a
-        self.b = b
+        self.a = IEEE(a)
+        self.b = IEEE(b)
         self.sign_bit = None
         self.mantis = None
         self.exponent = None
@@ -57,9 +62,20 @@ class Div:  # a / b
             exit(4)
 
     def calc_mantis(self):
-        result = float_bin(self.a.mantis / self.b.mantis, 52)
-        self.mantis = result[2:]
-        # print('{}, {}, {}'.format(self.mantis, self.a.mantis, self.b.mantis))
+        count = 26
+        Q = self.a.mantis
+        M = self.b.mantis
+        A = ['0' * len(self.a.mantis)]
+        MSB = 51
+        for _ in range(count):
+            if A[0] == '1':
+                A = A[1:] + Q[0]
+                Q = Q[1:] + '0'
+                A = A + M  # TODO: binary addition
+            else:
+                A = A[1:] + Q[0]
+                Q = Q[1:] + '0'
+                A = A - M  # TODO: BINARY SUB
 
     def normalize(self):
         pass
@@ -68,7 +84,8 @@ class Div:  # a / b
         print(self.mantis)
         result = str(self.sign_bit) + bin(self.exponent) + self.mantis
         result = result.replace('0b', '')
-        return binaryToFloat(result)
+        # return binaryToFloat(result)
+        return result
 
 
 def getBin(x, is_positive=True):
@@ -89,37 +106,70 @@ def floatToBinary64(value):
     return getBin(val, is_positive=value > 0)
 
 
+def floatToBinary32(value):
+    double = floatToBinary64(value)
+    single = double[0: 9] + double[MANTIS_INDEX: MANTIS_INDEX + 23]
+    return single
+
+
 def binaryToFloat(value):
     hx = hex(int(value, 2))
     return struct.unpack("d", struct.pack("q", int(hx, 16)))[0]
 
 
-def float_bin(number, places=3):    # 1.75 -> 1.11
-    whole, dec = str(number).split(".")
-    whole = int(whole)
-    dec = int(dec)
+# def float_bin(number, places=3):    # 1.75 -> 1.11
+#     whole, dec = str(number).split(".")
+#     whole = int(whole)
+#     dec = int(dec)
 
-    res = bin(whole).lstrip("0b") + "."
-    for _ in range(places):
-        whole, dec = str((decimal_converter(dec)) * 2).split(".")
-        dec = int(dec)
-        res += whole
+#     res = bin(whole).lstrip("0b") + "."
+#     for _ in range(places):
+#         whole, dec = str((decimal_converter(dec)) * 2).split(".")
+#         dec = int(dec)
+#         res += whole
 
-    return res
+#     return res
 
 
-def decimal_converter(num):
-    while num > 1:
-        num /= 10
-    return num
+# def decimal_converter(num):
+#     while num > 1:
+#         num /= 10
+#     return num
+
+
+def generate_test():
+    f64 = open("./input/input64.txt", "w")
+    f32 = open("./input/input32.txt", "w")
+    f10 = open("./input/input10.txt", "w")
+
+    out64 = open("./output/output64.txt", "w")
+    out10 = open("./output/output10.txt", "w")
+
+    for _ in range(10):
+        a = random.uniform(0, 10)
+        b = random.uniform(0, 10)
+        f10.write(str(a) + '\n' + str(b) + '\n')
+        f32.write(floatToBinary32(a) + '\n' + floatToBinary32(b) + '\n')
+        f64.write(floatToBinary64(a) + '\n' + floatToBinary64(b) + '\n')
+
+        div = Div(a, b)
+        result64 = div.divison()
+        result10 = a / b
+        out64.write(result64 + '\n')
+        out10.write(str(result10) + '\n')
+
+    f64.close()
+    f32.close()
+    f10.close()
+    out64.close()
+    out10.close()
 
 
 # print(int('11100000000000000000000000000000000000000000000000000', 2))
 # print(int('11000000000000000000000000000000000000000000000000000', 2))
-a = IEEE(3.5)
-b = IEEE(1.5)
-div = Div(a, b)
-result = div.divison()
-print(result)
-
-# print(float_bin(1.1666666666666667, 52))
+# a = IEEE(3.5)
+# b = IEEE(1.5)
+# div = Div(a, b)
+# result = div.divison()
+# print(result)
+generate_test()

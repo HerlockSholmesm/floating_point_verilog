3938198ed1fe0077783f6a2dde034825cbe58aaa
Masmae <57064253+Masmae@users.noreply.github.com>
2020-08-03 14:50:33 +0430

    Update div_sp.v

--- div/div_sp.v
+++ div/div_sp.v
@@ -1,5 +1,5 @@
 module div_sp(
-        input clk,
+        input clk, start,
         input [31:0] i_a, i_b,
         input reset,
         output [31:0] o_z);
@@ -7,13 +7,15 @@ module div_sp(
   reg a_sign, b_sign, z_sign;
   reg [22:0] a_mantis, b_mantis, z_mantis;
   reg [7:0] a_exp, b_exp, z_exp;
-  reg [22:0] A,M,Q;
+  reg [48:0] A,M;
+  reg [47:0] Q;
   reg [3:0] count;
   reg [1:0] state = 0;
   reg [31:0] z;
   parameter S_START = 2'b00,
             S_DIVIDE = 2'b01,
-            S_FINISH = 2'b10;
+            S_FINISH = 2'b10,
+            S_IDLE = 2'b11;
 
   always @(posedge clk)
   begin
@@ -30,9 +32,9 @@ module div_sp(
           b_sign <= i_b[31];
           a_exp <= i_a[30:23] - 127;
           b_exp <= i_b[30:23] - 127;
-          Q <= i_a[22:0];
+          Q <= {1'b1,i_a[22:0]} << 24;
           A <= 0;
-          M <= i_b[22:0];
+          M <= {1'b1,i_b[22:0]};
           count <= 0;
           //a=NaN or b=NaN -> z=NaN
           if ((a_exp==128 && a_mantis!=0) || (b_exp==128 && b_mantis!=0))
@@ -65,19 +67,17 @@ module div_sp(
 
         S_DIVIDE:
         begin
-        if (A[22])
+        if (A[48])
           begin
-            A = {A[21:0],Q[22]};
-            Q = {Q[21:0],1'b0};
+            {A,Q} = {A,Q} << 1;
             A = A+M;
           end
         else
           begin
-            A = {A[21:0],Q[22]};
-            Q = {Q[21:0],1'b0};
+            {A,Q} = {A,Q} << 1;
             A = A-M;
           end
-        if (A[22])
+        if (A[48])
           begin
             Q[0]=0;
           end
@@ -85,19 +85,53 @@ module div_sp(
           begin
             Q[0] =1;
           end
-        if (A[22])
+          if (A[48])
           begin
-            A = {A[21:0],Q[22]};
-            Q = {Q[21:0],1'b0};
+            {A,Q} = {A,Q} << 1;
             A = A+M;
           end
         else
           begin
-            A = {A[21:0],Q[22]};
-            Q = {Q[21:0],1'b0};
+            {A,Q} = {A,Q} << 1;
             A = A-M;
           end
-        if (A[22])
+        if (A[48])
+          begin
+            Q[0]=0;
+          end
+        else
+          begin
+            Q[0] =1;
+          end
+          if (A[48])
+          begin
+            {A,Q} = {A,Q} << 1;
+            A = A+M;
+          end
+        else
+          begin
+            {A,Q} = {A,Q} << 1;
+            A = A-M;
+          end
+        if (A[48])
+          begin
+            Q[0]=0;
+          end
+        else
+          begin
+            Q[0] =1;
+          end
+          if (A[48])
+          begin
+            {A,Q} = {A,Q} << 1;
+            A = A+M;
+          end
+        else
+          begin
+            {A,Q} = {A,Q} << 1;
+            A = A-M;
+          end
+        if (A[48])
           begin
             Q[0]=0;
           end
@@ -108,30 +142,32 @@ module div_sp(
           count = count+1;
           if (count == 12)
             state = S_FINISH;
-          else
-            state = S_DIVIDE;
         end
 
         S_FINISH:
         begin
-          if(~Q[22])
+          if(~Q[25])
           begin
-            Q <= {Q[21:0],1'b0};
+            Q <= Q << 1;
             z_exp <= z_exp-1;
           end
           //underflow,overflow
-          z_mantis = Q;
-          z_exp = a_exp - b_exp;
-          z_exp = z_exp + 127;
-          z_sign = a_sign ^ b_sign;
-          z = {z_sign,z_exp,z_mantis};
+          z_mantis <= Q[24:1];
+          z_exp <= 127 + a_exp - b_exp;
+          z_sign <= a_sign ^ b_sign;
+          state <= S_IDLE;
         end
 
+      S_IDLE:
+      begin
+        if (start == 1)
+          state = S_START;
+      end
       endcase
     end
     else
     begin
-      state <= S_START;
+      state <= S_IDLE;
       z_exp <= 0;
       z_mantis <= 0;
       z_exp <= 0;
@@ -140,5 +176,5 @@ module div_sp(
 
   end
 
-assign o_z = z;
+assign o_z = {z_sign,z_exp,z_mantis};
 endmodule

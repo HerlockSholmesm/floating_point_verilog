db6a3166c49b92f9ffb83ab5434f2d02d1030973
Abolfazl Rahimi <45298966+abolfazlrahimi@users.noreply.github.com>
2020-08-05 20:32:16 +0430

    A1_without_golden_model
    first sub project without golden model.

---
+++ sqrt/single precision/sp_sqrt_tb.v
@@ -0,0 +1,28 @@
+module sp_sqrt_tb;
+
+
+
+reg [31:0]in;
+wire [31:0]out;
+
+dp_sqrt s(.x(in), .y(out));
+
+
+integer out_file;
+integer i;
+initial begin
+	out_file = $fopen("sp_sim_out.txt", "w");
+	$fdisplay(out_file, "the current module name is = %m");
+	$fdisplay(out_file, "-----------------------------------------");
+	$timeformat(-9, 4, " ns", 5);
+
+	$fmonitor(out_file, "@%10g", $realtime, ": input is %h, and output is %h with remainder %h", in, out, s.r);
+
+	for(i = 100000; i > 0; i = i - 1) begin
+		in = $random;
+		#10;
+	end
+	$fclose(out_file);
+	$stop;
+end
+endmodule
